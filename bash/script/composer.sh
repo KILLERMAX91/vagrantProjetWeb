a7f73e6c75445c277466d92a6e1e8b52385136e5
KILLERMAX91 <mathieu.barata@gmail.com>
2017-04-26 00:47:54 +0200

    mise à jour du script bash

--- bash/script/composer.sh
+++ bash/script/composer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+if [ ! -f /usr/local/bin/composer ]; then
+    curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+fi
 
 eval cd /vagrant/web && composer update -vvv
 
